9faf6e474a1762d4680b6cab580d5372876f3600
Matteo Verzeletti <matteoverzeletti661@gmail.com>
2026-03-03 14:12:30 +0000

    Correct covariance, plot observable

--- analysis.py
+++ analysis.py
@@ -21,8 +21,8 @@ ANALYSIS_FILE='./an_file_hardcoded.yaml'
 POSTERIOR_NAME = 'BSZ-BqToK-wSR-wNFF-wCov'
 
 #OPTIONS---------------
-flag_sample = 1
-flag_predict = 0
+flag_sample = 0
+flag_predict = 1
 flag_gof = 0
 #-----------------------
 
@@ -38,7 +38,7 @@ time1 = time.perf_counter() - time0
 # 'BToK-Re(H0)', 'BToK-Im(H0)', 'BToK-Re(H0(F))', 'BToK-Im(H0(F))', 'BToK-Re(Delta-H0)', 'BToK-Im(Delta-H0)'
 
 if flag_predict:
-    predicts=['BToK-Re(H0(F))-over-F0', 'BToK-Im(H0(F))-over-F0'] 
+    predicts=['BToK-Re(H0)-over-F0', 'BToK-Im(H0)-over-F0'] 
     for i in predicts:
         eos.tasks.predict_observables(ANALYSIS_FILE, POSTERIOR_NAME, i,  base_directory=BASE_DIRECTORY)    
 
