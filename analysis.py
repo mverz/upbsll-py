663c14db3371e81155795a912b6ae0fedc90559f
Matteo Verzeletti <matteoverzeletti661@gmail.com>
2026-03-18 13:16:15 +0000

    test on local FF

--- analysis.py
+++ analysis.py
@@ -14,22 +14,23 @@ import time
 
 
 #BASE_DIRECTORY='./data_base'
+#BASE_DIRECTORY='./data_base_2'
 BASE_DIRECTORY='./test_base'
-#ANALYSIS_FILE='./an_file.yaml'
-ANALYSIS_FILE='./an_file_hardcoded.yaml'
+ANALYSIS_FILE='./an_file_BK.yaml'
+#ANALYSIS_FILE='./an_file_BKstar.yaml'
 
 POSTERIOR_NAME = 'BSZ-BqToK-wSR-wNFF-wCov'
 
 #OPTIONS---------------
 flag_sample = 1
-flag_predict = 0
+flag_predict = 1
 flag_gof = 0
 #-----------------------
 
 time0 = time.perf_counter()
 
 if flag_sample:
-    eos.tasks.sample_nested(ANALYSIS_FILE, POSTERIOR_NAME, base_directory=BASE_DIRECTORY, nlive=200, dlogz=1.0, seed=42)
+    eos.tasks.sample_nested(ANALYSIS_FILE, POSTERIOR_NAME, base_directory=BASE_DIRECTORY, nlive=100, dlogz=2.0, seed=42)
 
 time1 = time.perf_counter() - time0
 
@@ -38,7 +39,7 @@ time1 = time.perf_counter() - time0
 # 'BToK-Re(H0)', 'BToK-Im(H0)', 'BToK-Re(H0(F))', 'BToK-Im(H0(F))', 'BToK-Re(Delta-H0)', 'BToK-Im(Delta-H0)'
 
 if flag_predict:
-    predicts=['BToK-Re(H0)-over-F0', 'BToK-Im(H0)-over-F0'] 
+    predicts=['BToK-f+', 'BToK-f0', 'BToK-fT'] 
     for i in predicts:
         eos.tasks.predict_observables(ANALYSIS_FILE, POSTERIOR_NAME, i,  base_directory=BASE_DIRECTORY)    
 
