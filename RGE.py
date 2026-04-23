b91fa8141098f5548335b4fadb6b53f277073d45
Matteo Verzeletti <matteoverzeletti661@gmail.com>
2026-04-23 13:53:07 +0100

    RGE.py improvement

--- RGE.py
+++ RGE.py
@@ -20,11 +20,61 @@ pp = eos.Parameters.Defaults()
 oo = eos.Options()
 mm = eos.Model.make('SM', pp, oo)
 
-print(f'c7 at 4.18 = {mm.wilson_coefficients_b_to_s(4.18, 1, False).c7()}, at 8.36 = {mm.wilson_coefficients_b_to_s(8.36, 1, False).c7()}, at 2.09 = {mm.wilson_coefficients_b_to_s(2.09, 1, False).c7()}\n\n')
-
 mu_b = 4.18
 
-MB=5.27931
+pp.set("mass::B_d", 5.27931)
+MB = pp['mass::B_d'].evaluate()
+
+#---
+
+BASE_DIRECTORY='./test_base'
+ANALYSIS_FILE='./an_file_BK.yaml'
+POSTERIOR_NAME = 'BSZ-BqToK-wSR-wNFF-wCov'
+
+bfp, gof = eos.tasks.find_mode(
+        ANALYSIS_FILE,
+        POSTERIOR_NAME,
+        BASE_DIRECTORY,
+        importance_samples=True,
+        label='EOS',
+        optimizations=50
+    )
+display(bfp)
+display(gof)
+
+for par, val in zip(bfp.analysis.varied_parameters, bfp.point):
+    pp.set(par.name(), float(val))
+
+
+opt = eos.Options({'form-factors':'BSZ2015', 'nonlocal-formfactor':'GRvDV2022order5', 'model':'SM'})
+
+def F(s):
+    kin = eos.Kinematics({"q2": float(s)})
+
+    re_obs = eos.Observable.make("B->K::F_plus(q2)", pp, kin, opt).evaluate()
+
+    return re_obs
+# def F(s):
+#     if s==-6:
+#         return 0.3090910
+#     elif s==6:
+#         return 0.5264856
+#     else:
+#         raise ValueError("s value not recognized")
+def FT(s):
+    kin = eos.Kinematics({"q2": float(s)})
+
+    re_obs = eos.Observable.make("B->K::F_plus_T(q2)", pp, kin, opt).evaluate()
+
+    return re_obs
+# def FT(s):
+#     if s==-6:
+#         return -0.05572973
+#     elif s==6:
+#         return 0.09951136
+#     else:
+#         raise ValueError("s value not recognized")
+
 
 #---
 
@@ -75,7 +125,7 @@ def DC7(s, mu): # which is purely NLO (alpha_s)
     # p.set('b->s::c2', 1.009)
     p.set('b->s::c1', np.real(wc.c1()))
     p.set('b->s::c2', np.real(wc.c2()))
-    p.set("mass::b(MSbar)", mm.m_b_msbar(mu))
+    #p.set("mass::b(MSbar)", mm.m_b_msbar(mu))
     p.set("mass::c", mc)
     p.set("sb::mu", mu)
     #alpha_s = 0.226964 # from Nico's Mathematica at mu_b = 4.18
@@ -119,7 +169,7 @@ def DC9(s, mu):
     # p.set('b->s::c2', 1.009)
     p.set('b->s::c1', np.real(wc.c1()))
     p.set('b->s::c2', np.real(wc.c2()))
-    p.set("mass::b(MSbar)", mm.m_b_msbar(mu))
+    #p.set("mass::b(MSbar)", mm.m_b_msbar(mu))
     p.set("mass::c", mc)
     p.set("sb::mu", mu)
     #alpha_s = 0.226964 # from Nico's Mathematica at mu_b = 4.18
@@ -150,47 +200,26 @@ def DC9(s, mu):
 
     return DC_Re + 1j * DC_Im
 
-F = [3.090910e-01,
-     5.264856e-01]
-
-FT = [-5.572973e-02,
-      9.951136e-02]
 
 def H(s, mu):
-    if(s == -6.0): i=0
-    elif(s == 0.0): i=1
-    elif(s == 6.0): i=2
-    else: raise ValueError("s value not recognized")
 
-    return -1/(16*np.pi**2) * ( s/(2*MB**2) * DC9(s, mu) * F[i] + mm.m_b_msbar(mu)/MB * DC7(s, mu) * FT[i] * tensor_current_evol(mm.alpha_s(mu_b), mm.alpha_s(mu)) )
+    return -1/(16*np.pi**2) * ( s/(2*MB**2) * DC9(s, mu) * F(s) + mm.m_b_msbar(mu)/MB * DC7(s, mu) * FT(s) * tensor_current_evol(mm.alpha_s(mu_b), mm.alpha_s(mu)) )
 
 
 
 def A_0L(s, mu):
-    if(s == -6.0): i=0
-    elif(s == 0.0): i=1
-    elif(s == 6.0): i=2
-    else: raise ValueError("s value not recognized")
 
     wc = wc_at(mu)
 
-    return (wc.c9()- wc.c10()) * F[i] + 2 * mm.m_b_msbar(mu)*MB / s * ( wc.c7() * FT[i] * tensor_current_evol(mm.alpha_s(mu_b), mm.alpha_s(mu)) - 16*np.pi**2 * MB/mm.m_b_msbar(mu) * H(s, mu) )
+    return (wc.c9()- wc.c10()) * F(s) + 2 * mm.m_b_msbar(mu)*MB / s * ( wc.c7() * FT(s) * tensor_current_evol(mm.alpha_s(mu_b), mm.alpha_s(mu)) - 16*np.pi**2 * MB/mm.m_b_msbar(mu) * H(s, mu) )
 
 def A_0L_local(s, mu):
-    if(s == -6.0): i=0
-    elif(s == 0.0): i=1
-    elif(s == 6.0): i=2
-    else: raise ValueError("s value not recognized")
 
     wc = wc_at(mu)
 
-    return (wc.c9()- wc.c10()) * F[i] + 2 * mm.m_b_msbar(mu)*MB / s * ( wc.c7() * FT[i] * tensor_current_evol(mm.alpha_s(mu_b), mm.alpha_s(mu)) )
+    return (wc.c9()- wc.c10()) * F(s) + 2 * mm.m_b_msbar(mu)*MB / s * ( wc.c7() * FT(s) * tensor_current_evol(mm.alpha_s(mu_b), mm.alpha_s(mu)) )
 
 def A_0L_nonlocal(s, mu):
-    if(s == -6.0): i=0
-    elif(s == 0.0): i=1
-    elif(s == 6.0): i=2
-    else: raise ValueError("s value not recognized")
 
     wc = wc_at(mu)
 
@@ -198,81 +227,67 @@ def A_0L_nonlocal(s, mu):
 
 
 def A_0R(s, mu):
-    if(s == -6.0): i=0
-    elif(s == 0.0): i=1
-    elif(s == 6.0): i=2
-    else: raise ValueError("s value not recognized")
 
     wc = wc_at(mu)
 
-    return (wc.c9()+ wc.c10()) * F[i] + 2 * mm.m_b_msbar(mu)*MB / s * ( wc.c7() * FT[i] * tensor_current_evol(mm.alpha_s(mu_b), mm.alpha_s(mu)) - 16*np.pi**2 * MB/mm.m_b_msbar(mu) * H(s, mu) )
+    return (wc.c9()+ wc.c10()) * F(s) + 2 * mm.m_b_msbar(mu)*MB / s * ( wc.c7() * FT(s) * tensor_current_evol(mm.alpha_s(mu_b), mm.alpha_s(mu)) - 16*np.pi**2 * MB/mm.m_b_msbar(mu) * H(s, mu) )
 
 def A_0R_local(s, mu):
-    if(s == -6.0): i=0
-    elif(s == 0.0): i=1
-    elif(s == 6.0): i=2
-    else: raise ValueError("s value not recognized")
 
     wc = wc_at(mu)
 
-    return (wc.c9()+ wc.c10()) * F[i] + 2 * mm.m_b_msbar(mu)*MB / s * ( wc.c7() * FT[i] * tensor_current_evol(mm.alpha_s(mu_b), mm.alpha_s(mu)) )
+    return (wc.c9()+ wc.c10()) * F(s) + 2 * mm.m_b_msbar(mu)*MB / s * ( wc.c7() * FT(s) * tensor_current_evol(mm.alpha_s(mu_b), mm.alpha_s(mu)) )
 
 def A_0R_nonlocal(s, mu):
-    if(s == -6.0): i=0
-    elif(s == 0.0): i=1
-    elif(s == 6.0): i=2
-    else: raise ValueError("s value not recognized")
 
     wc = wc_at(mu)
 
     return 2 * mm.m_b_msbar(mu)*MB / s * ( - 16*np.pi**2 * MB/mm.m_b_msbar(mu) * H(s, mu) )
 
+# L amplitude
 
-# for mu in [mu_b, mu_b*2, mu_b/2]:
-#     print(f"mu = {mu} GeV")
+# for mu in [mu_b/2, mu_b, mu_b*2]:
+#     print(f"\nmu = {mu} GeV")
 #     for s in [-6.0, 6.0]:
 #         print(f"s = {s} GeV^2: A_0L = {A_0L(s, mu)}, mod_sq = {abs(A_0L(s, mu))**2}")
 
-# for mu in [mu_b, mu_b*2, mu_b/2]:
-#     print(f"mu = {mu} GeV")
+# for mu in [mu_b/2, mu_b, mu_b*2]:
+#     print(f"\nmu = {mu} GeV")
 #     for s in [-6.0, 6.0]:
 #         print(f"s = {s} GeV^2: A_0L = {A_0L_local(s, mu)}, mod_sq = {abs(A_0L_local(s, mu))**2}")
 
-# for mu in [mu_b, mu_b*2, mu_b/2]:
-#     print(f"mu = {mu} GeV")
+# for mu in [mu_b/2, mu_b, mu_b*2]:
+#     print(f"\nmu = {mu} GeV")
 #     for s in [-6.0, 6.0]:
 #         print(f"s = {s} GeV^2: A_0L = {A_0L_nonlocal(s, mu)}, mod_sq = {abs(A_0L_nonlocal(s, mu))**2}")
 
+# R amplitude
 
-
-# print("\n\n")
-
-
-
-# for mu in [mu_b, mu_b*2, mu_b/2]:
-#     print(f"mu = {mu} GeV")
+# for mu in [mu_b/2, mu_b, mu_b*2]:
+#     print(f"\nmu = {mu} GeV")
 #     for s in [-6.0, 6.0]:
 #         print(f"s = {s} GeV^2: A_0R = {A_0R(s, mu)}, mod_sq = {abs(A_0R(s, mu))**2}")
 
-# for mu in [mu_b, mu_b*2, mu_b/2]:
-#     print(f"mu = {mu} GeV")
+# for mu in [mu_b/2, mu_b, mu_b*2]:
+#     print(f"\nmu = {mu} GeV")
 #     for s in [-6.0, 6.0]:
 #         print(f"s = {s} GeV^2: A_0R = {A_0R_local(s, mu)}, mod_sq = {abs(A_0R_local(s, mu))**2}")
 
-# for mu in [mu_b, mu_b*2, mu_b/2]:
-#     print(f"mu = {mu} GeV")
+# for mu in [mu_b/2, mu_b, mu_b*2]:
+#     print(f"\nmu = {mu} GeV")
 #     for s in [-6.0, 6.0]:
 #         print(f"s = {s} GeV^2: A_0R = {A_0R_nonlocal(s, mu)}, mod_sq = {abs(A_0R_nonlocal(s, mu))**2}")
 
 
+# PRINT NICO'S TABLE
 
-# for mu in [mu_b, mu_b*2, mu_b/2]:
-#     print(f"mu = {mu} GeV")
-#     for s in [-6.0, 6.0]:
-#         print(f"s = {s} GeV^2: |L|^2 + |R|^2 = {abs(A_0L(s, mu))**2 + abs(A_0R(s, mu))**2}")
-
-
-
-# for mu in [mu_b, mu_b*2, mu_b/2]:
-#     print(f"mu = {mu} GeV")
-#     print(f"C1 = {wc_at(mu).c1()}, C2 = {wc_at(mu).c2()}, C7 = {wc_at(mu).c7()}, C9 = {wc_at(mu).c9()}, C10 = {wc_at(mu).c10()}\n")
+for q2 in [-6.0, 1.0, 3.0, 6.0]:
+    if(False):
+        print(f"F({q2}) = {F(q2)}, FT({q2}) = {FT(q2)}\n")
+    for mu in [mu_b/2, mu_b, mu_b*2]:
+        if(True):
+            print(f"q2 = {q2} GeV^2, mu = {mu} GeV\n")
+            
+        print(f"AKL = {A_0L(q2, mu):<.6f}, ReRatio = {A_0L(q2, mu).real/A_0L(q2, mu_b).real:<.6f}, ImRatio = {A_0L(q2, mu).imag/A_0L(q2, mu_b).imag:<.6f}, "+
+              f"AKR = {A_0R(q2, mu):<.6f}, ReRatio = {A_0R(q2, mu).real/A_0R(q2, mu_b).real:<.6f}, ImRatio = {A_0R(q2, mu).imag/A_0R(q2, mu_b).imag:<.6f}, "+
+              f"|L|^2+|R|^2 = {abs(A_0L(q2, mu))**2 + abs(A_0R(q2, mu))**2:<.6f}, Ratio = {(abs(A_0L(q2, mu))**2 + abs(A_0R(q2, mu))**2)/(abs(A_0L(q2, mu_b))**2 + abs(A_0R(q2, mu_b))**2):<.6f}\n")
